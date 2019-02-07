bb6d2029b8ac367cc5cc59afbc309f6cab1adb6c
Zane Campbell <zanegitinit@gmail.com>
2019-02-07 15:07:37 -0800

    setup static dir for express

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 let app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express!</h1>');
     res.send({
@@ -23,4 +25,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
