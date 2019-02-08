4b287e87d5c9498495c21565f50ec5582c843ba2
Zane Campbell <zanegitinit@gmail.com>
2019-02-08 00:06:59 -0800

    added handlebar partials and helpers

--- server.js
+++ server.js
@@ -3,21 +3,28 @@ const hbs = require('hbs');
 
 let app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
-        welcomeMessage: 'Welcome to my website!',
-        currentYear: new Date().getFullYear()
+        welcomeMessage: 'Welcome to my website!'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-       pageTitle: 'About Page',
-       currentYear: new Date().getFullYear()
+       pageTitle: 'About Page'
     });
 });
 
